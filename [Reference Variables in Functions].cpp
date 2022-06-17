5b862dab50729ad36a70436ab5cc90674ae5fc2e
Ian Lee <vanilla.punches@gmail.com>
2022-06-16 22:36:37 -0400

    Additional codes

--- [Reference Variables in Functions].cpp
+++ [Reference Variables in Functions].cpp
@@ -2,26 +2,28 @@
 
 using namespace std;
 
-void swap(int &, int &);
+int swap(int &, int &);
 
 int main()
 {
     int a = 100;
     int b = 900;
-
-    swap(a, b);
+    int c = swap(a,b);
 
     cout << "a: " << a << endl;
     cout << "b: " << b << endl;
+    cout << "c: " << c << endl;
 
     return 0;
 }
 
-void swap (int &x, int &y)
+int swap (int &x, int &y)
 {
     int t;
     t = x;
     x = y;
     y = t;
+
+    return x;
 
 }
